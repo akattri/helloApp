a1e81ac69aff872b4528b11b48b295c64b08dddc
Akash <akashattri62@gmail.com>
2026-04-01 00:00:01 +0530

    Refactor helloApp to UC3HelloApp with command-line input

--- helloApp.java
+++ helloApp.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
-public class helloApp{
-    public static void main (String args[]){
-        Scanner userinput = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = userinput.nextLine();
+public class UC3HelloApp {
+    public static void main(String[] args) {
+        // Get name from command-line argument, default to "World" if not provided
+        String name = (args.length > 0) ? args[0] : "World";
+        
         System.out.println("Hello, " + name);
     }
 }
